3cb3d866c36bb7ddef7cb3c7570ed7ebfa312445
HR2047 <131344829+HR2047@users.noreply.github.com>
2024-04-30 20:54:58 +0900

    Update ContentView.swift

--- AgricolaAssistant/ContentView.swift
+++ AgricolaAssistant/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State var count = 0
+    @State var fence_mode = true
     @State var tileColors: [[Color]] = [
             [.brown, .brown, .green],
             [.green, .green, .green],
@@ -16,7 +17,26 @@ struct ContentView: View {
             [.green, .green, .green],
             [.green, .green, .green]
         ]
+    
+    @State var vertical_fence: [[Bool]] = [
+        [false, false, false, false],
+        [false, false, false, false],
+        [false, false, false, false],
+        [false, false, false, false],
+        [false, false, false, false]
+    ]
+    
+    @State var horizontal_fence: [[Bool]] = [
+        [false, false, false],
+        [false, false, false],
+        [false, false, false],
+        [false, false, false],
+        [false, false, false],
+        [false, false, false]
+    ]
+    
     let tile_space: CGFloat = 10 // CGFloat型の定数として宣言
+    let fence_space: CGFloat = 15
     var body: some View {
         VStack {
             //            Image(systemName: "globe")
@@ -42,7 +62,7 @@ struct ContentView: View {
                     Button { } label: { Text("収穫") }
                     Button { } label: { Text("食事") }
                     Button { } label: { Text("繁殖") }
-                    Button { } label: { Text("柵") }
+                    Button { fence_mode.toggle() } label: { Text("柵") }
                     Button { } label: { Text("うまや") }
                     
                 }
@@ -68,34 +88,94 @@ struct ContentView: View {
             
             Spacer()
             
-            HStack(spacing: tile_space){
-                CustomRectangle(rectangle_color: $tileColors[0][0])
-                CustomRectangle(rectangle_color: $tileColors[0][1])
-                CustomRectangle(rectangle_color: $tileColors[0][2])
-            }
-            
-            HStack(spacing: tile_space) {
-                CustomRectangle(rectangle_color: $tileColors[1][0])
-                CustomRectangle(rectangle_color: $tileColors[1][1])
-                CustomRectangle(rectangle_color: $tileColors[1][2])
-            }
-            
-            HStack(spacing: tile_space) {
-                CustomRectangle(rectangle_color: $tileColors[2][0])
-                CustomRectangle(rectangle_color: $tileColors[2][1])
-                CustomRectangle(rectangle_color: $tileColors[2][2])
-            }
-            
-            HStack(spacing: tile_space) {
-                CustomRectangle(rectangle_color: $tileColors[3][0])
-                CustomRectangle(rectangle_color: $tileColors[3][1])
-                CustomRectangle(rectangle_color: $tileColors[3][2])
-            }
-            
-            HStack(spacing: tile_space) {
-                CustomRectangle(rectangle_color: $tileColors[4][0])
-                CustomRectangle(rectangle_color: $tileColors[4][1])
-                CustomRectangle(rectangle_color: $tileColors[4][2])
+            VStack(spacing: 0){
+                
+                HStack(spacing: fence_space){
+                    CustomFence(showFence: $horizontal_fence[0][0], horizontal: true, fmode: fence_mode)
+                    CustomFence(showFence: $horizontal_fence[0][1], horizontal: true, fmode: fence_mode)
+                    CustomFence(showFence: $horizontal_fence[0][2], horizontal: true, fmode: fence_mode)
+                }
+
+                
+                HStack(spacing: tile_space){
+                    CustomFence(showFence: $vertical_fence[0][0], horizontal: false, fmode: fence_mode)
+                    CustomRectangle(rectangle_color: $tileColors[0][0], fmode: fence_mode)
+                    CustomFence(showFence: $vertical_fence[0][1], horizontal: false, fmode: fence_mode)
+                    CustomRectangle(rectangle_color: $tileColors[0][1], fmode: fence_mode)
+                    CustomFence(showFence: $vertical_fence[0][2], horizontal: false, fmode: fence_mode)
+                    CustomRectangle(rectangle_color: $tileColors[0][2], fmode: fence_mode)
+                    CustomFence(showFence: $vertical_fence[0][3], horizontal: false, fmode: fence_mode)
+                }
+                
+                HStack(spacing: fence_space){
+                    CustomFence(showFence: $horizontal_fence[1][0], horizontal: true, fmode: fence_mode)
+                    CustomFence(showFence: $horizontal_fence[1][1], horizontal: true, fmode: fence_mode)
+                    CustomFence(showFence: $horizontal_fence[1][2], horizontal: true, fmode: fence_mode)
+                }
+                
+                HStack(spacing: tile_space) {
+                    CustomFence(showFence: $vertical_fence[1][0], horizontal: false, fmode: fence_mode)
+                    CustomRectangle(rectangle_color: $tileColors[1][0], fmode: fence_mode)
+                    CustomFence(showFence: $vertical_fence[1][1], horizontal: false, fmode: fence_mode)
+                    CustomRectangle(rectangle_color: $tileColors[1][1], fmode: fence_mode)
+                    CustomFence(showFence: $vertical_fence[1][2], horizontal: false, fmode: fence_mode)
+                    CustomRectangle(rectangle_color: $tileColors[1][2], fmode: fence_mode)
+                    CustomFence(showFence: $vertical_fence[1][3], horizontal: false, fmode: fence_mode)
+                }
+                
+                HStack(spacing: fence_space){
+                    CustomFence(showFence: $horizontal_fence[2][0], horizontal: true, fmode: fence_mode)
+                    CustomFence(showFence: $horizontal_fence[2][1], horizontal: true, fmode: fence_mode)
+                    CustomFence(showFence: $horizontal_fence[2][2], horizontal: true, fmode: fence_mode)
+                }
+                
+                HStack(spacing: tile_space) {
+                    CustomFence(showFence: $vertical_fence[2][0], horizontal: false, fmode: fence_mode)
+                    CustomRectangle(rectangle_color: $tileColors[2][0], fmode: fence_mode)
+                    CustomFence(showFence: $vertical_fence[2][1], horizontal: false, fmode: fence_mode)
+                    CustomRectangle(rectangle_color: $tileColors[2][1], fmode: fence_mode)
+                    CustomFence(showFence: $vertical_fence[2][2], horizontal: false, fmode: fence_mode)
+                    CustomRectangle(rectangle_color: $tileColors[2][2], fmode: fence_mode)
+                    CustomFence(showFence: $vertical_fence[2][3], horizontal: false, fmode: fence_mode)
+                }
+                
+                HStack(spacing: fence_space){
+                    CustomFence(showFence: $horizontal_fence[3][0], horizontal: true, fmode: fence_mode)
+                    CustomFence(showFence: $horizontal_fence[3][1], horizontal: true, fmode: fence_mode)
+                    CustomFence(showFence: $horizontal_fence[3][2], horizontal: true, fmode: fence_mode)
+                }
+                
+                HStack(spacing: tile_space) {
+                    CustomFence(showFence: $vertical_fence[3][0], horizontal: false, fmode: fence_mode)
+                    CustomRectangle(rectangle_color: $tileColors[3][0], fmode: fence_mode)
+                    CustomFence(showFence: $vertical_fence[3][1], horizontal: false, fmode: fence_mode)
+                    CustomRectangle(rectangle_color: $tileColors[3][1], fmode: fence_mode)
+                    CustomFence(showFence: $vertical_fence[3][2], horizontal: false, fmode: fence_mode)
+                    CustomRectangle(rectangle_color: $tileColors[3][2], fmode: fence_mode)
+                    CustomFence(showFence: $vertical_fence[3][3], horizontal: false, fmode: fence_mode)
+                }
+                
+                HStack(spacing: fence_space){
+                    CustomFence(showFence: $horizontal_fence[4][0], horizontal: true, fmode: fence_mode)
+                    CustomFence(showFence: $horizontal_fence[4][1], horizontal: true, fmode: fence_mode)
+                    CustomFence(showFence: $horizontal_fence[4][2], horizontal: true, fmode: fence_mode)
+                }
+                
+                HStack(spacing: tile_space) {
+                    CustomFence(showFence: $vertical_fence[4][0], horizontal: false, fmode: fence_mode)
+                    CustomRectangle(rectangle_color: $tileColors[4][0], fmode: fence_mode)
+                    CustomFence(showFence: $vertical_fence[4][1], horizontal: false, fmode: fence_mode)
+                    CustomRectangle(rectangle_color: $tileColors[4][1], fmode: fence_mode)
+                    CustomFence(showFence: $vertical_fence[4][2], horizontal: false, fmode: fence_mode)
+                    CustomRectangle(rectangle_color: $tileColors[4][2], fmode: fence_mode)
+                    CustomFence(showFence: $vertical_fence[4][3], horizontal: false, fmode: fence_mode)
+                }
+                
+                HStack(spacing: fence_space){
+                    CustomFence(showFence: $horizontal_fence[5][0], horizontal: true, fmode: fence_mode)
+                    CustomFence(showFence: $horizontal_fence[5][1], horizontal: true, fmode: fence_mode)
+                    CustomFence(showFence: $horizontal_fence[5][2], horizontal: true, fmode: fence_mode)
+                }
             }
             
         }
