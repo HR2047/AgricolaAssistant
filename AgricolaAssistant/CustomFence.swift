fd8ac08e2df7e71edf5e95ffbb2348d9412b1810
HR2047 <131344829+HR2047@users.noreply.github.com>
2024-04-30 20:54:14 +0900

    Add files via upload

---
+++ AgricolaAssistant/CustomFence.swift
@@ -0,0 +1,38 @@
+//
+//  CustomFence.swift
+//  AgricolaAssistant
+//
+//  Created by 廣澤凜太郎 on 2024/04/30.
+//
+
+import SwiftUI
+
+struct CustomFence: View {
+    @Binding var showFence: Bool
+    let horizontal: Bool
+    let fmode: Bool
+    
+    var body: some View {
+        VStack {
+            if horizontal {
+                Rectangle()
+                    .foregroundStyle(showFence ? Color.brown : Color.white)
+                    .frame(width: UIScreen.main.bounds.width / 4.5, height: 5)
+                    .padding(.top, 5)
+                    .onTapGesture {
+                        showFence.toggle() // showFenceをトグルする
+                    }
+                    .disabled(!fmode) // showFenceがfalseの場合はジェスチャーを無効にする
+            } else {
+                Rectangle()
+                    .foregroundStyle(showFence ? Color.brown : Color.white)
+                    .frame(width: 5, height: UIScreen.main.bounds.width / 4.5)
+                    .padding(.top, 5)
+                    .onTapGesture {
+                        showFence.toggle() // showFenceをトグルする
+                    }
+                    .disabled(!fmode) // showFenceがfalseの場合はジェスチャーを無効にする
+            }
+        }
+    }
+}
